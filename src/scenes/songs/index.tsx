54894b24dd6bc7b21a38a60dba80453f652e75f6
phbru <philippe.brunet.494@gmail.com>
2023-08-13 08:25:59 -0400

    song data

---
+++ src/scenes/songs/index.tsx
@@ -0,0 +1,54 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+//import {tokens} from "../..theme";
+import { mockData } from "../../data/songsData";
+import { AdminPanelSettingsOutlines } from "@mui/icons-material";
+
+//..others
+
+const Songs = () => {
+  // const theme = useTheme();
+  // const colors = tokens(theme.palette.mode)
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Song",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "phone",
+      headerName: "Phone",
+      flex: 1,
+    },
+    {
+      field: "phone",
+      headerName: "Phone",
+      flex: 1,
+    },
+    {
+      field: "acess",
+      headerName: "Acess",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <Box>
+      {/* <Header title="SONGS" subtitle="Managing the songs" /> */}
+      <Box>
+        <DataGrid rows={mockData} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Songs;
