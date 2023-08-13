eaccaf5e012e6bdc81e4769621e3c4ecdd3af42a
phbru <philippe.brunet.494@gmail.com>
2023-08-13 16:01:42 -0400

    using a list and handling the check

--- src/interfaces/SongListElementState.ts
+++ src/interfaces/SongListElementState.ts
@@ -1,8 +1,28 @@
 import { Song } from "./Song";
 
+// export class SongListElementState {
+//   songName: string;
+//   checked: boolean;
+//   content: Song;
+//   priority: number | undefined;
+
+//   constructor(
+//     songName: string,
+//     checked: boolean,
+//     content: Song,
+//     priority?: number
+//   ) {
+//     this.songName = songName;
+//     this.checked = checked;
+//     this.content = content;
+//     if (priority) this.priority = priority;
+//   }
+
+// }
+
 export interface SongListElementState {
   songName: string;
   checked: boolean;
   content: Song;
-  priority: number;
+  priority: number | undefined;
 }
--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -2,48 +2,90 @@ import { songsData } from "../../data/songsData";
 import { Song } from "../../interfaces/Song";
 import Checkbox from "@mui/material/Checkbox";
 import { useState } from "react";
+import { SongListElementState } from "../../interfaces/SongListElementState";
 
 interface SongComponentProps {
   song: Song;
 }
 
-const SongComponent = (props: SongComponentProps) => {
-  const [checked, setChecked] = useState(false);
+// const SongComponent = (props: SongComponentProps) => {
+//   const [checked, setChecked] = useState(false);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-    console.log(checked ? "checked" : "unchecked");
-  };
+//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setChecked(event.target.checked);
+//     console.log(checked ? "checked" : "unchecked");
+//   };
 
-  return (
-    <div className="song-component">
-      <h4 className="song-component__name">
-        {props.song.name} <Checkbox checked={checked} onChange={handleChange} />
-      </h4>
-      {Object.entries(props.song.performers).map(([instrument, performer]) => (
-        <ul key={instrument}>
-          <li>
-            <strong>{instrument} :</strong>
-            {performer}
-          </li>
-        </ul>
-      ))}
-    </div>
-  );
-};
+//   return (
+//     <div className="song-component">
+//       <h4 className="song-component__name">
+//         {props.song.name} <Checkbox checked={checked} onChange={handleChange} />
+//       </h4>
+//       {Object.entries(props.song.performers).map(([instrument, performer]) => (
+//         <ul key={instrument}>
+//           <li>
+//             <strong>{instrument} :</strong>
+//             {performer}
+//           </li>
+//         </ul>
+//       ))}
+//     </div>
+//   );
+// };
 
 const Dashboard = () => {
-  const songNames: string[] = Object.keys(songsData);
-  const [checkedSongs, setCheckedSongs] = useState(
-    Array(songNames.length).fill(false)
-  );
+  const songListState: Array<SongListElementState> = [];
+
+  for (const [songName, songContent] of Object.entries(songsData)) {
+    songListState.push({
+      songName: songName,
+      checked: false,
+      content: songContent,
+      priority: undefined,
+    });
+  }
+
+  const [songsList, setSongsList] =
+    useState<Array<SongListElementState>>(songListState);
+
+  const handleCheck = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    songIndex: number
+  ) => {
+    const updatedSongs = [...songsList];
+    updatedSongs[songIndex].checked = event.target.checked;
+    setSongsList(updatedSongs); // Assuming you're using React state to manage songsList
+    console.log(songsList);
+  };
 
   return (
     <div>
       <div className="song-section">
-        {songNames.map((songName) => (
-          <SongComponent song={songsData[songName]} />
+        {songsList.map((song, index) => (
+          <div className="song-component" key={index}>
+            <h4 className="song-component__name">
+              {song.songName}
+              <Checkbox
+                checked={song.checked}
+                onChange={(event) => handleCheck(event, index)}
+              />
+            </h4>
+            {Object.entries(song.content.performers).map(
+              ([instrument, performer]) => (
+                <ul key={instrument}>
+                  <li>
+                    <strong>{instrument} :</strong>
+                    {performer}
+                  </li>
+                </ul>
+              )
+            )}
+          </div>
         ))}
+
+        {/* {songNames.map((songName) => (
+          <SongComponent song={songsData[songName]} />
+        ))} */}
       </div>
     </div>
   );
