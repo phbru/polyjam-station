6ee4b2ae0daf290534b59b1f83852bad0202441b
phbru <philippe.brunet.494@gmail.com>
2023-08-21 09:45:39 -0400

    SongsForDateContext in place

--- src/components/SongsForADateSection.tsx
+++ src/components/SongsForADateSection.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+import { SongsForPickedDateContext } from "../contexts/SongsForPickedDateContext";
+import { SongForDate } from "../interfaces/SongForDate";
 import DatePickerBox from "./DatePickerBox";
 import SongsForPickedDateBox from "./SongsForPickedDateBox";
 
 const SongsForADateSection = () => {
+  const [possibleSongsForDate, setPossibleSongsForDate] = useState<
+    SongForDate[]
+  >([]);
+
   return (
     <div className="songs-for-a-date-section">
-      <DatePickerBox />
-      <SongsForPickedDateBox />
+      <SongsForPickedDateContext.Provider
+        value={{ possibleSongsForDate, setPossibleSongsForDate }}
+      >
+        <DatePickerBox />
+        <SongsForPickedDateBox />
+      </SongsForPickedDateContext.Provider>
     </div>
   );
 };
---
+++ src/contexts/SongsForPickedDateContext.ts
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+import { SongForDate } from "../interfaces/SongForDate";
+
+export interface SongsForPickedDateContextProps {
+  possibleSongsForDate: SongForDate[];
+  setPossibleSongsForDate: React.Dispatch<React.SetStateAction<SongForDate[]>>;
+}
+
+export const SongsForPickedDateContext =
+  createContext<SongsForPickedDateContextProps>({
+    possibleSongsForDate: [],
+    setPossibleSongsForDate: () => {},
+  });
