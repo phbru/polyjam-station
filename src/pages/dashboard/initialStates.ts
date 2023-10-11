f3323285e326fb0a3e88d0639194c9b557480027
phbru <philippe.brunet.494@gmail.com>
2023-10-10 20:38:59 -0400

    Fixed initial state

--- src/pages/dashboard/initialStates.ts
+++ src/pages/dashboard/initialStates.ts
@@ -7,19 +7,19 @@ import {
 
 import { SelectableSong } from "./sections/selectSongsSection/interfaces/SelectableSong";
 import { findPossibleIntervals } from "./helpers";
-import { availabilitiesData } from "../../data/availabilitiesData";
+
 import { DailyPossibleInterval } from "../../types/DailyPossibleInterval";
 import { AvailabilitiesByDates } from "../../interfaces/AvailabilitiesByDates";
 
 export const songListState: Array<SelectableSong> = [];
 export const allMusicians: Set<string> = new Set();
-export const initialPossibleIntervals: Array<DailyPossibleInterval> =
-  findPossibleIntervals(availabilitiesData, new Set());
 
 const parsedData = await parseCsvFile("./data/dispos.csv");
 export const availabilitiesByDates: AvailabilitiesByDates =
   convertToAvailabilitiesByDates(parsedData);
 console.log("availabilitiesByDates : ", availabilitiesByDates);
+export const initialPossibleIntervals: Array<DailyPossibleInterval> =
+  findPossibleIntervals(availabilitiesByDates, new Set());
 
 const parsedSongs = await parseSongs("./data/songs.csv");
 export const songDataByNames = convertToSongDataByNames(parsedSongs);
